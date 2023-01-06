c653b3de4e3b209bbd620831d7f7b516f5e14fda
Martin Mwangi <mwangimartin1904@gmail.com>
2023-01-06 08:12:02 +0300

    Scroll sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const ref = useRef(null);
 
+    const options = {
+        smooth: true,
+    };
+
+    return (
+        <LocomotiveScrollProvider options={options} containerRef={ref}>
+            <main data-scroll-container ref={ref}>
+                <section
+                    className="intro"
+                    data-scroll //This attribute makes this section an independent scrollable container
+                    data-scroll-speed="4"
+                    data-scroll-section
+                >
+                    <h1>This is the Introduction section</h1>
+                </section>
+
+                <section className="contents" data-scroll-section>
+                    <h1
+                        data-scroll
+                        data-scroll-direction="horizontal"
+                        data-scroll-speed="9"
+                    >
+                        I Love React
+                    </h1>
+                    <h1
+                        data-scroll
+                        data-scroll-direction="vertical"
+                        data-scroll-speed="9" // Values provided here affect the animations
+                    >
+                        That's why I code every day
+                    </h1>
+                </section>
+
+                <section id="stick" data-scroll-section>
+                    <h1
+                        data-scroll
+                        data-scroll-speed="5"
+                        data-scroll-sticky // Attibute that enables the sticky scroll
+                        data-scroll-target="#stick"
+                    >
+                        Hey I'm Sticky
+                    </h1>
+                    <p>other contents</p>
+                    <p>other contents</p>
+                    <p>other contents</p>
+                    <p>other contents</p>
+                    <p>other contents</p>
+                    <p>other contents</p>
+                    <p>other contents</p>
+                    <p>other contents</p>
+                </section>
+
+                <section className="footer" data-scroll-section>
+                    <h1
+                        className="op-class"
+                        data-scroll
+                        data-scroll-class="fadeIn"
+                        data-scroll-repeat="true"
+                        data-scroll-speed="2"
+                    >
+                        Let's end the application with this Footer
+                    </h1>
+                </section>
+            </main>
+        </LocomotiveScrollProvider>
+    );
+}
 export default App;
